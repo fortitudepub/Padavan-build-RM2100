640886eef0f8b63ae8a3349401da414612161c47
fortitude.zhang <fortitude.zhang@gmail.com>
2021-12-15 00:09:23 +0800

    add a fix script that should be added as a cron job to fix padwan gfw mode.

---
+++ fix-gfwlist.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+
+cmd=`sysctl -a | grep wmem | grep 8388`
+if [[ $? != 0 ]]; then
+    logger -t "fix-gfwlist" "optimize longfat network for office router."
+    sysctl -w net.ipv4.tcp_wmem="8388608 16777216 33554432"
+    sysctl -w net.ipv4.tcp_rmem="8388608 16777216 33554432"
+    sysctl -w net.ipv4.tcp_window_scaling=7
+    sysctl -w net.ipv4.tcp_adv_win_scale=1
+fi
+
+# check whitelist is ok by checking a random china prefix.
+cmd=`ipset list whitelist | grep 171.40.0.0`
+if [[ $? != 0 ]]; then
+    logger -t "fix-gfwlist" "whitelist not contain china ip, need fix."
+    # no need, ssr plus will do this.
+    #echo "create whitelist hash:net family inet" >/tmp/chnroutefix.ipset
+    if [[ ! -f /etc/storage/rawchnroute.data ]]; then
+        curl -4sSkL 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' -o /etc/storage/rawchnroute.data
+    fi
+    cat /etc/storage/rawchnroute.data | grep CN | grep ipv4 | awk -F'|' '{printf("add whitelist %s/%d\n", $4, 32-log($5)/log(2))}' >>/tmp/chnroutefix.ipset
+    #ipset destroy whitelist
+    # try create if not created.
+    ipset create whitelist hash:net family inet
+    ipset -R < /tmp/chnroutefix.ipset
+    rm /tmp/chnroutefix.ipset
+    logger -t "fix-gfwlist" "whitelist propogated."
+fi
+
+cmd=`iptables -t nat -L -nv | grep white | grep "\!"`
+if [[ $? != 0 ]]; then
+    logger -t "fix-gwflist" "can't find fix gfwlist rule, redo it."
+    cmd=`iptables -t nat -L -nv | grep white`
+    if [[ $? == 0 ]]; then
+        logger -t "fix-gfwlist" "required tables exist, need add missing rule."
+        iptables -t nat -A SS_SPEC_WAN_AC -m set ! --match-set whitelist dst -j SS_SPEC_WAN_FW
+    fi
+fi
